74c159907c0e9980ee326e547514ba889d6e243c
KK-Sequeira <kkseck@gmail.com>
2022-10-01 14:07:02 -0300

    inicial

---
+++ mysql/user_system.sql
@@ -0,0 +1,13 @@
+USE db;
+
+CREATE TABLE tbUser(
+    uId int not null AUTO_INCREMENT,
+    uName varchar(100) NOT NULL,
+    uMail varchar(50) NOT NULL,
+    uNuser varchar(12) NOT NULL,
+    uPw varchar(8) NOT NULL,
+    PRIMARY KEY (uId)
+);
+
+INSERT INTO tbUser(uName, uMail, uNuser, uPw) 
+VALUES("Carlinhos", "kkseck@gmail.com", "kk", "pw1234");
